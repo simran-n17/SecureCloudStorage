b3ebdbd3e4542b263660c50b5a9c054f996fe448
simran_n17 <simrannegi1537@gmail.com>
2025-07-26 20:26:36 +0530

    updated

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 AWS_ACCESS_KEY_ID = 'YOUR ACCESS ID'
 AWS_SECRET_ACCESS_KEY = 'YOUR SECRET ACCESS ID'
 AWS_BUCKET_NAME = 'YOUR BUCKET NAME'  # replace with your actual bucket name
-REGION_NAME = 'BUCKET REGION'  # Mumbai region
+REGION_NAME = 'BUCKET REGION'  #  region
 
 PUBLIC_KEY_PATH = 'keys/public.pem'
 PRIVATE_KEY_PATH = 'keys/private.pem'
